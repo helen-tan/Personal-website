a7335a676679bf8669987f96b7b44b7d7f34917f
Glitch (far-mud-cord) <none>
2020-10-20 06:10:00 +0000

    🚶🐑 Checkpoint ./public/scripts/client.js:23306375/581

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,6 @@
 // Your application Javascript
+
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".slider");
+  var instances = M.Slider.init(elems, {});
+});
